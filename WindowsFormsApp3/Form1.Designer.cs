1ef040655d3cde1a5628e11abdf79b50c6e5983b
Ben Cohen <ben.cohenvi@gmail.com>
2017-09-19 22:15:33 +0300

    Start Of AI
    Starter

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.T1 = new System.Windows.Forms.Button();
+            this.T2 = new System.Windows.Forms.Button();
+            this.T3 = new System.Windows.Forms.Button();
+            this.M1 = new System.Windows.Forms.Button();
+            this.M2 = new System.Windows.Forms.Button();
+            this.M3 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // T1
+            // 
+            this.T1.AccessibleName = "T1";
+            this.T1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.T1.Location = new System.Drawing.Point(12, 12);
+            this.T1.Name = "T1";
+            this.T1.Size = new System.Drawing.Size(75, 75);
+            this.T1.TabIndex = 0;
+            this.T1.UseVisualStyleBackColor = true;
+            this.T1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // T2
+            // 
+            this.T2.AccessibleName = "T2";
+            this.T2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.T2.Location = new System.Drawing.Point(93, 12);
+            this.T2.Name = "T2";
+            this.T2.Size = new System.Drawing.Size(75, 75);
+            this.T2.TabIndex = 1;
+            this.T2.UseVisualStyleBackColor = true;
+            this.T2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // T3
+            // 
+            this.T3.AccessibleName = "T3";
+            this.T3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.T3.Location = new System.Drawing.Point(174, 12);
+            this.T3.Name = "T3";
+            this.T3.Size = new System.Drawing.Size(75, 75);
+            this.T3.TabIndex = 2;
+            this.T3.UseVisualStyleBackColor = true;
+            this.T3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // M1
+            // 
+            this.M1.AccessibleName = "M1";
+            this.M1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.M1.Location = new System.Drawing.Point(12, 93);
+            this.M1.Name = "M1";
+            this.M1.Size = new System.Drawing.Size(75, 75);
+            this.M1.TabIndex = 3;
+            this.M1.UseVisualStyleBackColor = true;
+            this.M1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // M2
+            // 
+            this.M2.AccessibleName = "M2";
+            this.M2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.M2.Location = new System.Drawing.Point(93, 93);
+            this.M2.Name = "M2";
+            this.M2.Size = new System.Drawing.Size(75, 75);
+            this.M2.TabIndex = 4;
+            this.M2.UseVisualStyleBackColor = true;
+            this.M2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // M3
+            // 
+            this.M3.AccessibleName = "M3";
+            this.M3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.M3.Location = new System.Drawing.Point(179, 93);
+            this.M3.Name = "M3";
+            this.M3.Size = new System.Drawing.Size(75, 75);
+            this.M3.TabIndex = 5;
+            this.M3.UseVisualStyleBackColor = true;
+            this.M3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // B1
+            // 
+            this.B1.AccessibleName = "B1";
+            this.B1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B1.Location = new System.Drawing.Point(12, 174);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(75, 75);
+            this.B1.TabIndex = 6;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // B2
+            // 
+            this.B2.AccessibleName = "B2";
+            this.B2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B2.Location = new System.Drawing.Point(93, 174);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(75, 75);
+            this.B2.TabIndex = 7;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // B3
+            // 
+            this.B3.AccessibleName = "B3";
+            this.B3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B3.Location = new System.Drawing.Point(174, 174);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(75, 75);
+            this.B3.TabIndex = 8;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 267);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.M3);
+            this.Controls.Add(this.M2);
+            this.Controls.Add(this.M1);
+            this.Controls.Add(this.T3);
+            this.Controls.Add(this.T2);
+            this.Controls.Add(this.T1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button T1;
+        private System.Windows.Forms.Button T2;
+        private System.Windows.Forms.Button T3;
+        private System.Windows.Forms.Button M1;
+        private System.Windows.Forms.Button M2;
+        private System.Windows.Forms.Button M3;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true;
+        int turn_count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (turn)
+                btn.Text = "X";
+            else
+                btn.Text = "O";
+            btn.Enabled = false;
+            this.turn = !this.turn;
+            this.turn_count++;
+            check_Winner();
+        }
+
+
+        private void check_Winner()
+        {
+            bool winner = false;
+            if ((T1.Text == T2.Text) && (T2.Text == T3.Text) && (!T1.Enabled)) 
+                winner = true;
+            if ((M1.Text == M2.Text) && (M2.Text == T3.Text) && (!M1.Enabled))
+                winner = true;
+            if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                winner = true;
+            if ((T1.Text == M1.Text) && (M1.Text == B1.Text) && (!T1.Enabled))
+                winner = true;
+            if ((T2.Text == M2.Text) && (M2.Text == B2.Text) && (!T2.Enabled))
+                winner = true;
+            if ((T3.Text == M3.Text) && (M3.Text == B3.Text) && (!T3.Enabled))
+                winner = true;
+            if ((T1.Text == M2.Text) && (M2.Text == B3.Text) && (!T1.Enabled))
+                winner = true;
+            if ((T3.Text == M2.Text) && (M2.Text == B1.Text) && (!T3.Enabled))
+                winner = true;
+            if (winner)
+            {
+                disable_buttons();
+                string win = "";
+                if (turn)
+                    win = "O";
+                else
+                    win = "X";
+                MessageBox.Show(win + " Won!", "Score");
+            }
+            else if (this.turn_count == 9)
+            {
+                disable_buttons();
+                MessageBox.Show("It's a Tie!", "Score");
+            }
+                    
+        }
+
+        private void disable_buttons()
+        {
+            foreach (Control c in Controls)
+            {
+                Button btn = c as Button;
+                btn.Enabled = false;
+            }
+        }
+    }
+}
